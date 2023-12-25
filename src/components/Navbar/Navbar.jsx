51a778c1db2c120b3c6c41d66edfb0659e00cb2a
Mugunth148 <Mugunth148@gmail.com>
2023-12-25 14:59:36 +0530

    update: font format

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,13 +23,30 @@ const Navbar = () => {
             }}
           >
             <div className="logo-container">
-              <span><svg height="1.5em" width="1.5em" fill="#3353d3" viewBox="0 0 1024 1089.841" xmlns="http://www.w3.org/2000/svg"><path d="M520.258 1056.92H503.76c0-134.61-50.306-252.21-150.918-352.824S134.628 553.178 0 553.178V536.68c134.628 0 252.23-50.306 352.842-151.145C453.454 284.677 503.76 167.075 503.76 32.938h16.498c0 134.61 50.306 252.21 150.918 352.824S889.39 536.68 1024 536.68v16.498c-134.61 0-252.21 50.306-352.824 150.918S520.258 922.31 520.258 1056.92z"/></svg></span>
-              
-              <a href="#" data-text="Mugunth" style={{
-                color: menuOpen ? "var(--primary)" : "var(--secondary)",
-                fontWeight: menuOpen ? "600" : "600",
-                transitionDelay: menuOpen ? "0.1s" : "0.5s",
-              }}> Coded by</a>
+              <span>
+                <svg
+                  height="1.5em"
+                  width="1.5em"
+                  fill="#3353d3"
+                  viewBox="0 0 1024 1089.841"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M520.258 1056.92H503.76c0-134.61-50.306-252.21-150.918-352.824S134.628 553.178 0 553.178V536.68c134.628 0 252.23-50.306 352.842-151.145C453.454 284.677 503.76 167.075 503.76 32.938h16.498c0 134.61 50.306 252.21 150.918 352.824S889.39 536.68 1024 536.68v16.498c-134.61 0-252.21 50.306-352.824 150.918S520.258 922.31 520.258 1056.92z" />
+                </svg>
+              </span>
+
+              <a
+                href="#"
+                data-text="Mugunth"
+                style={{
+                  color: menuOpen ? "var(--primary)" : "var(--secondary)",
+                  fontWeight: menuOpen ? "600" : "600",
+                  transitionDelay: menuOpen ? "0.1s" : "0.5s",
+                }}
+              >
+                {" "}
+                Coded by
+              </a>
             </div>
           </div>
           <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
